3cfa7c94ae51dcdcf03cf9c43bf1a6a2412efa46
gfabao <78553522+gfabao@users.noreply.github.com>
2021-06-05 13:41:35 -0300

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,4 @@
+#veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u REisKvW9Ym4X9p3RuqrV4VrjuUySrBHVNq.0005 -p x --cpu 4
